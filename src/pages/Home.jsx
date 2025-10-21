e9f96ea858d82ea9cbc2e8b745dbe4286a4d9c16
nomanqadir-cosmoinc <nomanqadir@cosmoinc.co>
2025-10-21 12:01:07 +0500

    update layout and add routing

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="min-h-screen w-full bg-amber-900 flex items-center justify-center">
+      <h2 className="text-center">home</h2>
+    </div>
+  );
+}
+
+export default Home;
