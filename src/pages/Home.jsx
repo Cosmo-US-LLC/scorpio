815fc9c058ff761c4f00e8a0fcfb581ea460228d
faheem303 <faheemrehmat360@gmail.com>
2025-10-21 12:35:38 +0500

    layout setup

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import Herosection from "./herosection";
 
 function Home() {
   return (
-    <div className="min-h-screen w-full bg-amber-900 flex items-center justify-center">
-      <h2 className="text-center">home</h2>
+    <div className="min-h-screen w-full">
+     <Herosection />
     </div>
   );
 }
